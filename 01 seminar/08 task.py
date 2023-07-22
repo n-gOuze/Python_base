eef8b438b41e2c8bb803ab6a7e39ecfa8ac7513e
Guseva Natalia <natalia.gyseva@gmail.com>
2023-07-22 13:44:42 +0300

    added task 08 teacher solution

--- 01 seminar/08 task.py
+++ 01 seminar/08 task.py
@@ -5,8 +5,17 @@
 # 3 2 4 -> yes
 # 3 2 1 -> no
 
-width = int(input())
-length = int(input())
-slice = int(input())
-if slice < width * length and (slice % width == 0 or slice % length == 0): print('yes')
-else: print('no')
+# width = int(input())
+# length = int(input())
+# slice = int(input())
+# if slice < width * length and (slice % width == 0 or slice % length == 0): print('yes')
+# else: print('no')
+
+# Решение преподавателя
+n = int(input())
+m = int(input())
+k = int(input())
+
+if (k % n == 0 or k % m == 0) and k < m * n:
+    print ("YES")
+else: print ("NO")
