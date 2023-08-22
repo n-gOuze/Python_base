3cd645fd5ea1c027c030be1d45af31981ea71226
Guseva Natalia <natalia.gyseva@gmail.com>
2023-08-22 21:32:39 +0300

    added task 24 solution

--- 04 seminar/22 task.py
+++ 04 seminar/22 task.py
@@ -16,4 +16,4 @@ m = input().split()
 first = [int(i) for i in input().split()]
 second = [int(j) for j in input().split()]
 
-print(*sorted(set(first).intersection(second))) #intersection - пересечения
+print(*sorted(set(first).intersection(second))) #intersection - пересечения * - элемент распаковки списка
--- 04 seminar/24 task.py
+++ 04 seminar/24 task.py
@@ -15,3 +15,27 @@
 # Пример:
 # 4 -> 1 2 3 4
 # 9
+
+n = int(input())
+bushes = [int(i) for i in input().split()]
+bush_max = 0
+
+for i in range(n):
+    bush_sum = bushes[i - 1] + bushes[i] + bushes[i + 1 if i < n - 1 else 0]
+    if bush_sum > bush_max:
+        bush_max = bush_sum
+
+print(bush_max)
+
+# Решение 2
+
+n = int(input())
+bushes = [int(i) for i in input().split()]
+bush_max = 0
+
+for i in range(-1, n - 1):
+    bush_sum = bushes[i - 1] + bushes[i] + bushes[i + 1]
+    if bush_sum > bush_max:
+        bush_max = bush_sum
+
+print(bush_max)
