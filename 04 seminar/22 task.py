35a338fbf60802d59cbd63c525b5d6043a6ae7ab
Guseva Natalia <natalia.gyseva@gmail.com>
2023-08-22 21:11:05 +0300

    added task 22 solution

--- 04 seminar/22 task.py
+++ 04 seminar/22 task.py
@@ -10,3 +10,10 @@
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
+
+n = input().split()
+m = input().split()
+first = [int(i) for i in input().split()]
+second = [int(j) for j in input().split()]
+
+print(*sorted(set(first).intersection(second))) #intersection - пересечения
