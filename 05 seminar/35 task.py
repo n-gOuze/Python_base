f32f0ee3c995e53efb03bb86adebb706a5007f71
Guseva Natalia <natalia.gyseva@gmail.com>
2023-09-03 14:55:19 +0300

    added task 35 solution

---
+++ 05 seminar/35 task.py
@@ -0,0 +1,19 @@
+# Задача №35. 
+# Напишите функцию, которая принимает одно число и проверяет, является ли оно простым
+# Напоминание: Простое число - это число, которое имеет 2 делителя: 1 и n(само число)
+# Input: 5
+# Output: yes 
+
+def simple(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# print(simple(3))
+
+for i in range(101):
+    if simple(i):
+        print(i)
