abf9a22ade571784cc63872b8d56e24c935114f6
Guseva Natalia <natalia.gyseva@gmail.com>
2023-09-03 16:16:04 +0300

    added task 28 solution

--- 05 seminar/28 task.py
+++ 05 seminar/28 task.py
@@ -5,3 +5,14 @@
 # Пример
 # 2 2
 # 4
+
+def f(a, b):
+    if b < 0 < a:
+        a, b = b , a
+    if b == 0:
+        return a
+    return f(a + 1, b - 1)
+
+n = int(input())
+m = int(input())
+print(f(n, m))
