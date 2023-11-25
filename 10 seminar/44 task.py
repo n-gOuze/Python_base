c698ffd1ddce63ddadfce308a9d71bff12cac212
Guseva Natalia <natalia.gyseva@gmail.com>
2023-11-25 15:14:28 +0300

    added task 44 solution

--- 10 seminar/44 task.py
+++ 10 seminar/44 task.py
@@ -1,4 +1,23 @@
 # Задача №44
+# Создать новый столбец height_group в таблице с пингвинами, 
+# который будет отвечать за показатель длины клюва пингвина. 
+# high - высокий(от 42), middle - средний(от 35 до 42), low - низкий(до 35).
+# Изобразить гистограмму по flipper_length_mm с оттенком height_group.
+
+import seaborn as sns
+peng = sns.load_dataset('penguins')
+peng.head()
+
+peng.loc[peng['bill_length_mm'] <= 35, 'height_group'] = 'low'
+peng.loc[(peng['bill_length_mm'] > 35) & (peng['bill_length_mm'] <= 42),
+'height_group'] = 'middle'
+peng.loc[peng['bill_length_mm'] > 42, 'height_group'] = 'high'
+
+peng.head()
+
+sns.histplot(data=peng, x='flipper_length_mm', hue="height_group")
+
+# Задача №44 (дополнительное)
 # В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. 
 # Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
 # import random
